fb1a2b5eab837cffd684243eaa46ee6a3bb93618
sangho Kim <sy980527@naver.com>
2022-05-13 17:04:34 +0900

    Simple calculation

---
+++ _ch02/src/Ex2_1.java
@@ -0,0 +1,26 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("add");
+		System.out.println("3+5");
+		System.out.println(3+5);
+		System.out.println("");
+		
+		System.out.println("sub");
+		System.out.println("3-5");
+		System.out.println(3-5);
+		System.out.println("");
+		
+		System.out.println("multiplication");
+		System.out.println("3*5");
+		System.out.println(3*5);
+		System.out.println("");
+		
+		System.out.println("division");
+		System.out.println("3/5");		
+		System.out.println(3/5);				
+	}
+}
