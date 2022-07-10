51ae70e5b5f574b4de1a4d2ca03b2644ff666878
sangho527 <sy980527@naver.com>
2022-07-10 22:51:17 +0900

    변수

---
+++ src/variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args) {
+        int x = 5; // int x; 와 x = 5; 를 한줄로 합침
+        System.out.println(x); // 화면에 x의 값인 5가 출력
+
+        x = 10; // 변수 x 에 10 저장 , 기존에 있던 5는 지워짐.
+        System.out.println(x); // x의 값인 10이 출력
+    }
+}
