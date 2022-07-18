a91e423d4e1902dc2f9e39ec73618079ca7de071
sangho527 <sy980527@naver.com>
2022-07-18 21:53:04 +0900

    static 메소드

---
+++ src/Soojebimain.java
@@ -0,0 +1,11 @@
+class Soojebi{
+    static void print(){
+        System.out.println("static method");
+    }
+}
+
+public class Soojebimain{
+    public static void main(String[] args) {
+        Soojebi.print();
+    }
+}
