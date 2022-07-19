a0abc81626093c734c85bc7e4409e1d26e7c46f7
sangho527 <sy980527@naver.com>
2022-07-19 12:27:13 +0900

    break문

---
+++ src/repeat.java
@@ -0,0 +1,13 @@
+public class repeat {
+
+        public static void main(String[] args) {
+            for(int i=1; i<5; i++){
+                if(i==3) break;
+                System.out.println(i);
+            }
+
+            System.out.println("반복문 끝!");
+        }
+    }
+
+
