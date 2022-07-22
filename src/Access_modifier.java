bf99e460ed10ffa8d0ed84df7e0a5b9f509b46eb
sangho527 <sy980527@naver.com>
2022-07-22 20:00:59 +0900

    접근자

--- src/Access_modifier.java
+++ src/Access_modifier.java
@@ -1,16 +1,22 @@
+import java.sql.SQLOutput;
+
 public class Access_modifier {
     String name = "홍길동";
     public int age = 40;
-    protected  int height = 170;
+    protected  int height = 178;
     private int weight = 68;
+
+
 }
 class Child extends Access_modifier{
     public static void main(String[] args) {
         Child c = new Child();
+        String sex = "m";
 
         System.out.println(c.name);
         System.out.println(c.age);
         System.out.println(c.height);
+        System.out.println(sex);
         //System.out.println(c.weight); // weight 는 private
     }
 }
