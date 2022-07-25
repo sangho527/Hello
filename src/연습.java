473fbe2d112c6c1693251060a1a7a143416fefd9
sangho527 <sy980527@naver.com>
2022-07-26 01:05:15 +0900

    배열

--- src/연습.java
+++ src/연습.java
@@ -1,17 +1,19 @@
-class A{
-    static void f() {
-        System.out.println("1");
+public class 연습{
+    static int nSize = 4;
 
+    public static void main(String[] args) {
+        int[] arr = new int[nSize];
+        makeArray(arr);
+        
+        for(int i = 0; i<nSize; i++) {
+            System.out.println(arr[i] + "");
+        }
+    }
+
+    public static void makeArray(int[] args) {
+        for(int i = 0; i<nSize; i++) {
+            int[] arr = new int[0];
+            arr[i] = i;
+        }
+    }
 }
-
-    class B extends A {
-        static void f() {
-            System.out.println("3");
-        }
-
-        public class C {
-            public void main(String[] args) {
-                A a = new B();
-                a.f();
-            }
-        }
