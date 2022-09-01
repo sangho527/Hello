7f274df69d320c863b34f3b7cbd2945dbcf3456c
sangho527 <sy980527@naver.com>
2022-09-01 17:23:05 +0900

    백엔드스쿨

--- src/문제3.java
+++ src/문제3.java
@@ -1,5 +1,35 @@
-public class 문제3 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class 문제3 {
+
+    //금액을 넣었을때 ex) 120000원을 넣었을때 50000원 두장 10000원 두장이 나와야하는
+
+        public static void main(String[] args) {
+        int money;
+
+
+
+        Scanner sc = new Scanner(System.in); // System.in 사용자로부터 입력을 받기위한 입력 스트림
+        System.out.print("금액을 넣으세요: " );
+        money = sc.nextInt();
+
+        System.out.println("50000원" + money/50000 + "개");
+                money = money % 50000;
+        System.out.println("10000원" + money/10000 + "개");
+                money = money % 10000;
+        System.out.println("5000원" + money/5000 + "개");
+                money = money % 5000;
+        System.out.println("1000원" + money/1000 + "개");
+                money = money % 1000;
+        System.out.println("500원" + money/500 + "개");
+                money = money % 500;
+        System.out.println("100원" + money/100 + "개");
+                money = money % 100;
+        System.out.println("50원" + money/50 + "개");
+                money = money % 50;
+        System.out.println("10원" + money/10 + "개");
+
+
+        
     }
 }
---
+++ src/문제4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class 문제4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1.전체리스트 2.등록 0.종료");
+
+    }
+}
