dafa1733ffdbc3ab9bca418a6b3cf27ecef650f6
sangho527 <sy980527@naver.com>
2022-09-04 19:43:54 +0900

    백엔드스쿨

--- src/문제3.java
+++ src/문제3.java
@@ -9,12 +9,12 @@ public class 문제3 {
 
 
 
-        Scanner sc = new Scanner(System.in); // System.in 사용자로부터 입력을 받기위한 입력 스트림
+        Scanner sc = new Scanner(System.in); // 스캐너를 사용해 금액을 입력받고
         System.out.print("금액을 넣으세요: " );
-        money = sc.nextInt();
+        money = sc.nextInt(); // Money 객체에 입력받은값을 넣음
 
-        System.out.println("50000원" + money/50000 + "개");
-                money = money % 50000;
+        System.out.println("50000원" + money/50000 + "개"); // money 객체에서 50000을 나누고 몫을 받아 매수 출력
+                money = money % 50000; // money 값에 입력 받았던 금액에 %로 나눈 나머지를 저장 동일한 과정을 반복해 문제를 해결했습니다. 감사합니다.
         System.out.println("10000원" + money/10000 + "개");
                 money = money % 10000;
         System.out.println("5000원" + money/5000 + "개");
