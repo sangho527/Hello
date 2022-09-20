c1211e93040bb03bcc8dd226ce6d76591cd1042a
sangho527 <sy980527@naver.com>
2022-09-20 22:44:25 +0900

    int, boolean

---
+++ src/world.java
@@ -0,0 +1,10 @@
+public class world {
+    public static void main(String[] args) {
+        int num1, num2;
+        num1 = 1;
+        num2 = 2;
+
+        System.out.println(num1<num2); // 불린으로 출력
+    }
+
+}
