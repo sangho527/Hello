0d61ff7499b678b32da9c199425dd86b768b7d9d
sangho527 <sy980527@naver.com>
2022-09-20 22:44:03 +0900

    백엔드스쿨

--- src/문제3.java
+++ src/문제3.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class 문제3 {
 
-    //금액을 넣었을때 ex) 120000원을 넣었을때 50000원 두장 10000원 두장이 나와야하는
 
         public static void main(String[] args) {
         int money;
 
 
 
-        Scanner sc = new Scanner(System.in); // 스캐너를 사용해 금액을 입력받고
+        Scanner sc = new Scanner(System.in);
         System.out.print("금액을 넣으세요: " );
-        money = sc.nextInt(); // Money 객체에 입력받은값을 넣음
+        money = sc.nextInt();
 
-        System.out.println("50000원" + money/50000 + "개"); // money 객체에서 50000을 나누고 몫을 받아 매수 출력
-                money = money % 50000; // money 값에 입력 받았던 금액에 %로 나눈 나머지를 저장 동일한 과정을 반복해 문제를 해결했습니다. 감사합니다.
+        System.out.println("50000원" + money/50000 + "개");
+                money = money % 50000;
         System.out.println("10000원" + money/10000 + "개");
                 money = money % 10000;
         System.out.println("5000원" + money/5000 + "개");
@@ -29,7 +28,5 @@ public class 문제3 {
                 money = money % 50;
         System.out.println("10원" + money/10 + "개");
 
-
-        
     }
 }
--- src/문제4.java
+++ src/문제4.java
@@ -10,12 +10,12 @@ import java.time.LocalDate;
 public class 문제4 {
     public static void main(String[] args) {
 
-        ArrayList<String> title = new ArrayList<String>(); // 어레이리스트를 통해 객체를 저장하게 했습니다.
+        ArrayList<String> title = new ArrayList<String>();
         String name = null;
-        LocalDate now = LocalDate.now(); // 로컬데이트 api를 사용해 현재 날짜 출력
+        LocalDate now = LocalDate.now();
         int size = title.size();
-        String newline = System.getProperty("line.separator"); // 개행문자를 사용해 게시판 글 목록을 아래로 내려줍니다.
-        title.add(0, ""); // 0번째 인덱스에 공백을 넣어 1부터 출력이 되게합니다.
+        String newline = System.getProperty("line.separator");
+        title.add(0, "");
 
         
 
@@ -24,7 +24,7 @@ public class 문제4 {
         boolean bool = true;
 
 
-        while (bool) { // while문 무한루프를 사용해 종료를 눌러야 나갈 수 있게 만듭니다.
+        while (bool) {
 
 
             System.out.println("1.전체리스트 2.등록 0.종료");
@@ -36,7 +36,7 @@ public class 문제4 {
 
                     System.out.println("== 게시물 리스트 ==");
                     System.out.println("no          " + "title                  " + "date");
-                    for (int i = 1; i <title.size(); i++){ //for 문을 사용해 게시물 번호를 갱신해줍니다.
+                    for (int i = 1; i <title.size(); i++){
                     System.out.printf("%d           %s            %s", i, title.get(i), now + newline);
                     }
                     break;
@@ -56,7 +56,7 @@ public class 문제4 {
 
                 case 0: {
                     System.out.println("프로그램이 종료되었습니다.");
-                    bool = false; // false 논리 값을 통해 while문 종료
+                    bool = false;
                 }
 
 
