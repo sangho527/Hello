00696d301d87f1e4b21ed72acd5817fb20920ed7
sangho527 <sy980527@naver.com>
2022-09-21 15:41:21 +0900

    별찍기

---
+++ src/star2.java
@@ -0,0 +1,18 @@
+public class star2 {
+
+    public static void main(String[] args) {
+
+        int i, j;
+
+        for(i=5; i >= 0; i--){
+
+            for(j=0; j < i; j++) {
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
