0c88743c34825f045cc05ddbc0448b96621666c7
sangho527 <sy980527@naver.com>
2022-09-22 17:29:45 +0900

    클래스

---
+++ src/Hi_Everyone.java
@@ -0,0 +1,13 @@
+public class Hi_Everyone {
+    public static void main(String[] args) {
+        System.out.println("프로그램의 시작");
+        hiEveryone(12);
+        hiEveryone(13);
+        System.out.println("프로그램의 끝");
+    }
+
+    public static void hiEveryone(int age) {
+        System.out.println("좋은 아침 입니다.");
+        System.out.println("제 나이는"+ age +"세 입니다.");
+    }
+}
