fe9437102bc84f6ee8b46a3d348b65c387dea859
sangho527 <sy980527@naver.com>
2022-09-25 15:40:05 +0900

    자바 배열 입력받아 출력

--- src/연습.java
+++ src/연습.java
@@ -1,8 +1,20 @@
-class MyMath {
+import java.util.PrimitiveIterator;
+import java.util.Scanner;
 
-    long add(long a, long b) {
-        long result = a + b;
-        //return result;
-        return a + b;
+public class 연습 {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        int size = arr.length;
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i < size; i++){
+            System.out.println("숫자를 입력해주세요");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(arr[0] + "," + arr[1] + "," + arr[2] );
+
+
     }
 }
